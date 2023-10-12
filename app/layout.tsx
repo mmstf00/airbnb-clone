9a8b4360ce7409e2322c9618d95a6b456cbbc367
Mustafa <mustafa.ercan392@gmail.com>
2023-10-12 22:40:04 +0300

    Implement Search filters

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import ClientOnly from "./components/ClientOnly";
 import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import ToasterProvider from "./providers/ToasterProvider";
@@ -25,6 +26,7 @@ export default async function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <RentModal />
+          <SearchModal />
           <RegisterModal />
           <LoginModal />
         </ClientOnly>
